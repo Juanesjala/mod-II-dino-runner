923918d2e21409ecb2ca0131aabb504282002322
Juanesjala <126845320+Juanesjala@users.noreply.github.com>
2023-03-07 17:29:30 -0500

    ejercicios

---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,40 @@
+import pygame
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.birds import Birds
+from dino_runner.utils.constants import SMALL_CACTUS , LARGE_CACTUS , BIRD
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles = []
+        self.condicion = 0
+    def update(self, game):
+
+        
+        if len(self.obstacles) == 0:
+            if self.condicion == 0:
+                cactus = Cactus(SMALL_CACTUS)
+                self.obstacles.append(cactus)
+                self.condicion += 1
+            
+            elif self.condicion == 1:
+                birds = Birds(BIRD)
+                self.obstacles.append(birds)
+                self.condicion += 1
+                
+            elif self.condicion == 2:
+                cactus= Cactus(LARGE_CACTUS)
+                self.obstacles.append(cactus)
+                self.condicion = 0
+           
+        for obstacles in self.obstacles: 
+            obstacles.update(game.game_speed ,self.obstacles)
+            if game.player.dino_rect.colliderect(obstacles.rect):
+                pygame.time.delay(1000)
+                game.playing = False
+                break
+
+           
+                      
+    def draw(self,screen):
+        for obstacle in self.obstacles:
+           obstacle.draw(screen)
